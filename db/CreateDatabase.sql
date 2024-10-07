b598e53e174c88bd416fc0b2c8fb94ff13263aa5
cnmurakami <caionmurakami@gmail.com>
2024-10-07 20:14:30 -0300

    Corrigindo dump do bd

--- db/CreateDatabase.sql
+++ db/CreateDatabase.sql
@@ -4,7 +4,7 @@ USE `rscarautomotive`;
 --
 -- Host: localhost    Database: rscarautomotive
 -- ------------------------------------------------------
--- Server version	8.0.33
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -93,6 +93,16 @@ CREATE TABLE `peca` (
   PRIMARY KEY (`id_peca`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peca`
+--
+
+LOCK TABLES `peca` WRITE;
+/*!40000 ALTER TABLE `peca` DISABLE KEYS */;
+INSERT INTO `peca` VALUES (1,'Pastilha Diant Lado Direito',5,75.00),(2,'Pastilha Diant Lado Esquerdo',2,68.00),(3,'Pastilha Tras Lado Esquerdo',3,52.90),(4,'Pastilha Tras Lado Direito',7,46.70),(5,'Molas dianteiras',4,350.00),(6,'Molas traseiras',1,380.00),(7,'Lâmpada H7',15,30.00),(8,'Brucutu Esguicho do limpador',7,95.00);
+/*!40000 ALTER TABLE `peca` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping data for table `peca`
@@ -212,6 +222,257 @@ LOCK TABLES `veiculo` WRITE;
 /*!40000 ALTER TABLE `veiculo` DISABLE KEYS */;
 /*!40000 ALTER TABLE `veiculo` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'rscarautomotive'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `associar_tp_serv_ordem` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `associar_tp_serv_ordem`(
+		IN p_id_ordem INT,
+        IN p_id_servico INT
+)
+BEGIN
+		INSERT INTO tipo_servico_ordem (id_ordem, id_servico)
+        VALUES (p_id_ordem, p_id_servico);
+        
+        SELECT "Tipo de serviço por ordem associada com sucesso" AS message;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `inserir_cliente` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserir_cliente`(
+    IN p_cpf VARCHAR(14),
+    IN p_cnpj VARCHAR(18),
+    IN p_nome VARCHAR(100),
+    IN p_razao_social VARCHAR(100),
+    IN p_endereco VARCHAR(255),
+    IN p_telefone VARCHAR(20),
+    IN p_email VARCHAR(100)
+)
+BEGIN
+    INSERT INTO cliente (cpf, cnpj, nome, razao_social, endereco, telefone, email) 
+    VALUES (p_cpf, p_cnpj, p_nome, p_razao_social, p_endereco, p_telefone, p_email);
+    
+    SELECT 'Cliente inserido com sucesso!' AS message;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `inserir_ordem_s` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserir_ordem_s`(
+	IN p_id_cliente INT,
+    IN p_id_veiculo INT
+)
+BEGIN
+    INSERT INTO veiculo (id_cliente, id_veiculo) 
+    VALUES ( p_id_cliente, p_id_veiculo);
+    
+    SELECT 'Ordem inserida com sucesso!' AS message;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `inserir_tp_servico` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserir_tp_servico`(
+		IN p_nome VARCHAR(100),
+        IN p_valor DECIMAL(10, 2)
+)
+BEGIN
+		INSERT INTO tipo_servico (nome, valor)
+        VALUES (p_nome, p_valor);
+        
+        SELECT "Tipo de serviço incluído com sucesso!" AS message;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `inserir_veiculo` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserir_veiculo`(
+	IN p_id_cliente INT,
+    IN p_placa VARCHAR(10),
+    IN p_chassi VARCHAR(50),
+    IN p_marca VARCHAR(100),
+    IN p_modelo VARCHAR(100),
+    IN p_ano_fabricacao INT,
+    IN p_ano_modelo INT,
+    IN p_cor VARCHAR(50)
+)
+BEGIN
+    INSERT INTO veiculo (id_cliente, placa, chassi, marca, modelo, ano_fabricacao, ano_modelo, cor) 
+    VALUES ( p_id_cliente, p_placa, p_chassi, p_marca, p_modelo, p_ano_fabricacao, p_ano_modelo, p_cor);
+    
+    SELECT 'Veículo inserido com sucesso!' AS message;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `selecionar_cliente` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selecionar_cliente`(
+    IN p_nome VARCHAR(100),
+	IN p_razao_social VARCHAR(100),
+	IN p_email VARCHAR(100),
+	IN p_telefone VARCHAR(20),
+    IN p_cpf VARCHAR(14),
+    IN p_cnpj VARCHAR(18)
+)
+BEGIN
+	DECLARE cliente_encontrado INT;
+    DECLARE p_parametro_lower VARCHAR(100);
+    SET p_parametro_lower = LOWER(p_parametro);
+
+    SELECT id_cliente INTO cliente_encontrado 
+    FROM cliente 
+    WHERE LOWER(nome) LIKE CONCAT('%', p_parametro_lower, '%') 
+       OR LOWER(email) LIKE CONCAT('%', p_parametro_lower, '%') 
+       OR telefone LIKE CONCAT('%', p_parametro, '%') 
+       OR cpf LIKE CONCAT('%', p_parametro, '%') 
+       OR cnpj LIKE CONCAT('%', p_parametro, '%')
+    LIMIT 1;    
+    
+    IF cliente_encontrado IS NULL THEN
+        SELECT id_cliente INTO cliente_encontrado FROM cliente WHERE nome = p_nome;
+    END IF;
+    IF cliente_encontrado IS NULL THEN
+        SELECT id_cliente INTO cliente_encontrado FROM cliente WHERE razao_social = p_razao_social;
+    END IF;
+    IF cliente_encontrado IS NULL THEN
+        SELECT id_cliente INTO cliente_encontrado FROM cliente WHERE email = p_email;
+    END IF;
+    IF cliente_encontrado IS NULL THEN
+        SELECT id_cliente INTO cliente_encontrado FROM cliente WHERE telefone = p_telefone;
+    END IF;
+    IF cliente_encontrado IS NULL THEN
+        SELECT id_cliente INTO cliente_encontrado FROM cliente WHERE cpf = p_cpf;
+    END IF;
+    IF cliente_encontrado IS NULL THEN
+        SELECT id_cliente INTO cliente_encontrado FROM cliente WHERE cnpj = p_cnpj;
+    END IF;
+        
+		IF cliente_encontrado IS NOT NULL THEN
+		SELECT * FROM cliente WHERE id_cliente = cliente_encontrado;
+        ELSE 
+			SELECT 'Cliente não cadastrado na base' AS messagem;
+        END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `selecionar_veiculo` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selecionar_veiculo`(
+    IN p_placa VARCHAR(10),
+    IN id_cliente int
+)
+BEGIN
+	DECLARE veiculo_encontrado INT;
+	DECLARE p_parametro_lower VARCHAR(100);
+	SET p_parametro_lower = LOWER(p_parametro);
+    SELECT COUNT(*) INTO veiculo_encontrado 
+    FROM veiculo 
+    WHERE LOWER(id_veiculo) LIKE CONCAT('%', p_parametro_lower, '%') 
+       OR LOWER(placa) LIKE CONCAT('%', p_parametro_lower, '%') 
+       OR LOWER(chassi) LIKE CONCAT('%', p_parametro_lower, '%') 
+       OR id_cliente LIKE CONCAT('%', p_parametro, '%');
+       
+	IF veiculo_encontrado = 0 THEN
+        SELECT COUNT(*) INTO veiculo_encontrado FROM veiculo WHERE placa = p_placa;
+    END IF;
+    IF veiculo_encontrado = 0 THEN
+        SELECT COUNT(*) INTO veiculo_encontrado FROM veiculo WHERE id_cliente = p_id_cliente;
+    END IF;
+        
+    IF veiculo_encontrado >= 1 THEN
+        SELECT * FROM veiculo 
+        WHERE (LOWER(id_veiculo) LIKE CONCAT('%', p_parametro_lower, '%') 
+            OR LOWER(placa) LIKE CONCAT('%', p_parametro_lower, '%') 
+            OR LOWER(chassi) LIKE CONCAT('%', p_parametro_lower, '%') 
+            OR id_cliente LIKE CONCAT('%', p_parametro, '%'))
+            OR (placa = p_placa AND id_cliente = p_id_cliente);
+    ELSE 
+        SELECT 'Veículo não encontrado para o cliente especificado ou placa inválida' AS mensagem;
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -222,4 +483,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-03 22:12:13
+-- Dump completed on 2024-10-07 19:59:57
