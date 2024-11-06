9f24a00737c1d4d406eca4cfcf892dbccb0a89fc
Ellen dos Santos <ellen.xavier123@gmail.com>
2024-11-06 01:12:46 -0300

    Atualizando página de ordem
    Incluindo Soma e Subtração na página da ordem

--- static/ordem.js
+++ static/ordem.js
@@ -1,125 +1,117 @@
-function filtrarServicos(){
-    const searchInput = document.getElementById("filtrar_servicos");
+function filtrarServicos() {
+  const searchInput = document.getElementById("filtrar_servicos");
 
-    searchInput.onkeyup = function() {
-        const searchTerm = searchInput.value.toLowerCase();
-        const tableRows = document.querySelectorAll(".item_lista");
+  searchInput.onkeyup = function() {
+      const searchTerm = searchInput.value.toLowerCase();
+      const tableRows = document.querySelectorAll(".item_lista");
 
-        for (let i = 0; i < tableRows.length; i++) {
-            const row = tableRows[i];
-            const nameCell = row.querySelector("td:nth-child(2)");
-            if (nameCell){
-                const nameText = nameCell.textContent.toLowerCase();
-                
-                if (nameText.indexOf(searchTerm) !== -1) {
-                    row.style.display = "table-row";
-                }
-                else {
-                    row.style.display = "none";
-                }
-            }
-        }
-    };
+      for (let i = 0; i < tableRows.length; i++) {
+          const row = tableRows[i];
+          const nameCell = row.querySelector("td:nth-child(2)");
+          if (nameCell) {
+              const nameText = nameCell.textContent.toLowerCase();
+
+              if (nameText.indexOf(searchTerm) !== -1) {
+                  row.style.display = "table-row";
+              } else {
+                  row.style.display = "none";
+              }
+          }
+      }
+  };
 }
 
-const checkboxes = document.querySelectorAll("input[type='checkbox']");
-const selectedItemsTable = document.querySelector(".itens_selecionados");
+document.querySelectorAll('.servicos input[type="checkbox"]').forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+      const row = this.closest('tr');
+      const id = this.id;
+      const nome = row.querySelector('label[for="' + id + '"]').textContent.trim();
+      const valorUnitario = parseFloat(row.querySelector('td:nth-child(3)').textContent.replace(/[^\d.-]/g, '').trim());
+      const quantidadeSelecionada = this.checked ? 1 : 0;
 
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener("change", function(event) {
-    if (this.checked) {
-        const row = this.closest('tr');
-        const nameCell = row.querySelector('td:nth-child(2)');
-        const valueCell = row.querySelector('td:nth-child(3)');
-        const quantityCell = row.querySelector('td:nth-child(4)');
-
-        const name = nameCell.textContent.trim();
-        let value;
-        if (quantityCell) { 
-          const quantity = parseInt(quantityCell.textContent.trim());
-          value = parseFloat(valueCell.textContent.replace(/[^\d.-]/g, '').trim()) * quantity;
-        } else { 
-          value = parseFloat(valueCell.textContent.replace(/[^\d.-]/g, '').trim());
-        }
-        if (isNaN(value)) {
-          console.error('Valor inválido:', value);
-          return; 
-        }
-        const newRow = document.createElement("tr");
-        const nameCellElement = document.createElement("td");
-        nameCellElement.textContent = name;
-
-        const valueCellElement = document.createElement("td");
-        valueCellElement.textContent = "R$ " + value.toFixed(2);
-        newRow.appendChild(nameCellElement);
-        newRow.appendChild(valueCellElement);
-        newRow.id = Math.random().toString(36).substring(2, 15);
-
-        checkbox.setAttribute('data-row-id', newRow.id);
-        selectedItemsTable.appendChild(newRow);
-    } else if (this === event.target) {
-        const rowId = this.dataset.rowId;
-        const rowToRemove = selectedItemsTable.querySelector(`tr[id="${rowId}"]`);
-        if (rowToRemove) {
-            selectedItemsTable.removeChild(rowToRemove);
-            this.removeAttribute('data-row-id');
-        }
-    }
-
-    sumTableColumn('.itens_selecionados', 1);
+      updateSelectedItems(id, nome, valorUnitario, quantidadeSelecionada, 'servico');
+      sumTableColumn('.itens_selecionados', 1);
   });
 });
+
+const selectedItemsTable = document.querySelector(".itens_selecionados");
+
+document.querySelectorAll('.ajustar-quantidade').forEach(button => {
+  button.addEventListener('click', function() {
+      const row = this.closest('tr');
+      const id = row.dataset.id;
+      const nome = row.querySelector('td:nth-child(1)').textContent.trim(); // Ajustado para capturar o nome da peça corretamente
+      const valorUnitario = parseFloat(row.querySelector('td:nth-child(3)').textContent.replace(/[^\d.-]/g, '').trim());
+      const tipoItem = row.classList.contains('servico') ? 'servico' : 'peca';
+
+      const quantidadeSpan = row.querySelector('.quantidade-selecionada');
+      const estoqueCell = tipoItem === 'peca' ? row.querySelector('.estoque') : null;
+
+      let quantidadeSelecionada = parseInt(quantidadeSpan.textContent);
+      let estoque = estoqueCell ? parseInt(estoqueCell.textContent) : null;
+
+      if (this.textContent === '+') {
+          if (tipoItem === 'peca' && estoque > 0) {
+              quantidadeSelecionada++;
+              estoque--;
+          } else if (tipoItem === 'servico') {
+              quantidadeSelecionada++;
+          }
+      } else if (this.textContent === '-') {
+          if (quantidadeSelecionada > 0) {
+              quantidadeSelecionada--;
+              if (tipoItem === 'peca') estoque++;
+          }
+      }
+
+      quantidadeSpan.textContent = quantidadeSelecionada;
+      if (estoqueCell) estoqueCell.textContent = estoque;
+
+      updateSelectedItems(id, nome, valorUnitario, quantidadeSelecionada, tipoItem); 
+      sumTableColumn('.itens_selecionados', 1);
+  });
+});
+
+
+function updateSelectedItems(id, nome, valorUnitario, quantidade, tipoItem) {
+  let existingRow = selectedItemsTable.querySelector(`tr[data-id="${id}"]`);
+
+  if (quantidade > 0) {
+      const valorTotal = valorUnitario * quantidade;
+      if (!existingRow) {
+          existingRow = document.createElement("tr");
+          existingRow.setAttribute("data-id", id);
+          existingRow.classList.add(tipoItem);
+          existingRow.innerHTML = `<td>${nome}</td><td class="valor">R$ ${valorTotal.toFixed(2)}</td>`;
+          selectedItemsTable.appendChild(existingRow);
+      } else {
+          existingRow.querySelector(".valor").textContent = `R$ ${valorTotal.toFixed(2)}`;
+      }
+  } else if (existingRow) {
+      selectedItemsTable.removeChild(existingRow);
+  }
+}
 
 function sumTableColumn(table, columnIndex) {
   const tableElement = document.querySelector(table);
   const rows = tableElement.querySelectorAll("tr");
   let sum = 0;
 
-  for (let i = 0; i < rows.length; i++) {
-    const cells = rows[i].querySelectorAll("td");
-    if (cells.length > 1) {
-      const valueCell = cells[columnIndex];
-      const value = parseFloat(valueCell.textContent.replace(/[^\d.-]/g, '').trim());
-      if (!isNaN(value)) {
-        sum += value; 
+  rows.forEach(row => {
+      const valueCell = row.querySelectorAll("td")[columnIndex];
+      if (valueCell) {
+          const value = parseFloat(valueCell.textContent.replace(/[^\d.-]/g, ''));
+          if (!isNaN(value)) sum += value;
       }
-    }
-  }
+  });
 
-  document.getElementById('mostrarTotal').value = "R$ " + sum.toLocaleString(undefined, {minimumFractionDigits: 2}).toString();
+  document.getElementById('mostrarTotal').value = "R$ " + sum.toLocaleString(undefined, { minimumFractionDigits: 2 });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   sumTableColumn('.itens_selecionados', 1);
 }, false);
 
-document.addEventListener('change', function() {
-  sumTableColumn('.itens_selecionados', 1);
-});
-
-document.querySelectorAll('.ajustar-quantidade').forEach(button => {
-  button.addEventListener('click', function() {
-      const id = this.dataset.id;
-      let quantidade = parseInt(this.dataset.quantidade);
-      const valorUnitario = parseFloat(this.dataset.valor);
-      if (this.textContent === '+') {
-          if (quantidade > 0) {
-              quantidade++;
-              this.dataset.quantidade = quantidade;
-          }
-      } else if (this.textContent === '-') {
-          if (quantidade > 1) {
-              quantidade--;
-              this.dataset.quantidade = quantidade;
-          }
-      }
-      const quantidadeCell = document.querySelector(`#tabela_pecas tr[data-id="${id}"] .quantidade`);
-      quantidadeCell.textContent = quantidade;
-      const total = valorUnitario * quantidade;
-      sumTableColumn('.itens_selecionados', 1);
-      updatePieceTotal();
-  });
-});
 
 function enviar_form(){
   form = document.getElementById("ordem")
